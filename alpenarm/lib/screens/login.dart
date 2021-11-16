d8be2b5be114121fa0ec211ab667a2e0cf2542f5
Suman Adhikari <expertxkid@gmail.com>
2021-11-16 11:59:39 +0545

    Add widget elements for login form without styling

--- alpenarm/lib/screens/login.dart
+++ alpenarm/lib/screens/login.dart
@@ -1,10 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(
+        children: [
+          Image.asset(
+            'assets/mount-everest.jpg',
+            height: MediaQuery.of(context).size.height,
+            cacheHeight: MediaQuery.of(context).size.height.round(),
+            fit: BoxFit.cover,
+          ),
+          Positioned(
+            top: 62,
+            left: 62,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  cacheWidth: 38,
+                ),
+                Row(
+                  children: [
+                    Text('Alpen'),
+                    Text('Arm'),
+                  ],
+                ),
+                Text('In the name of health!'),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            bottom: 0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(32),
+                topRight: Radius.circular(32),
+              ),
+              child: Container(
+                height: 450,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 41,
+                    top: 57,
+                    left: 36,
+                    right: 36,
+                  ),
+                  child: Form(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Welcome'),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Email',
+                          ),
+                        ),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Password',
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize:
+                                Size(MediaQuery.of(context).size.width, 16),
+                          ),
+                          onPressed: () {},
+                          child: Text('Sign in'),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('Forgot Password?'),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize:
+                                Size(MediaQuery.of(context).size.width, 16),
+                          ),
+                          onPressed: () {},
+                          child: Text('Create Account'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
