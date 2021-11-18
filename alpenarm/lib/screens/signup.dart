758c33d6cee83ddf1c7c2d96b7b0434bea76d1dd
Suman Adhikari <expertxkid@gmail.com>
2021-11-18 12:24:23 +0545

    Remove single child scroll view and background colors

--- alpenarm/lib/screens/signup.dart
+++ alpenarm/lib/screens/signup.dart
@@ -11,89 +11,91 @@ class SignupScreen extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: [
+            Image.asset(
+              'assets/mountain.jpg',
+              height: MediaQuery.of(context).size.height,
+              cacheHeight: MediaQuery.of(context).size.height.round(),
+              // cacheWidth: MediaQuery.of(context).size.width.round(),
+              fit: BoxFit.cover,
+            ),
             Positioned(
               left: 0,
               bottom: 0,
-              child: SingleChildScrollView(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(32),
-                      topRight: Radius.circular(32),
-                    ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
                   ),
-                  width: MediaQuery.of(context).size.width,
-                  height: 520,
-                  child: Padding(
-                    padding: EdgeInsets.only(
-                      top: 54,
-                      bottom: 44,
-                      left: 36,
-                      right: 36,
-                    ),
-                    child: Form(
-                        child: Container(
-                      color: AppColors.white,
+                  color: AppColors.white,
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 520,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    top: 54,
+                    bottom: 44,
+                    left: 36,
+                    right: 36,
+                  ),
+                  child: Form(
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 20),
+                        width: double.infinity,
+                        child: const Text(
+                          'Create Account',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'First name'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Last name'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(labelText: 'Email'),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(
+                          bottom: 27,
+                          top: 40,
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Create Account',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 20),
-                            width: double.infinity,
-                            child: const Text(
-                              'Create Account',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                          TextFormField(
-                            decoration:
-                                InputDecoration(labelText: 'First name'),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(labelText: 'Last name'),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(labelText: 'Email'),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Password',
-                            ),
-                            obscureText: true,
-                            obscuringCharacter: '*',
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(
-                              bottom: 27,
-                              top: 40,
-                            ),
-                            child: ElevatedButton(
+                          Text('Already have an account?'),
+                          SizedBox(
+                            height: 24,
+                            child: TextButton(
                               onPressed: () {},
-                              child: const Text(
-                                'Create Account',
-                              ),
+                              child: Text('Sign In'),
                             ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Already have an account?'),
-                              SizedBox(
-                                height: 24,
-                                child: TextButton(
-                                  onPressed: () {},
-                                  child: Text('Sign In'),
-                                ),
-                              )
-                            ],
                           )
                         ],
-                      ),
-                    )),
-                  ),
+                      )
+                    ],
+                  )),
                 ),
               ),
             )
