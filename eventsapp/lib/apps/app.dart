3c00676f564dd54d4099dcab9f28061b4ad2f18d
Suman Adhikari <expertxkid@gmail.com>
2021-11-19 23:58:46 +0545

    Init app project structure

---
+++ eventsapp/lib/apps/app.dart
@@ -0,0 +1,14 @@
+import 'package:eventsapp/themes/light_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightTheme,
+      home: const Scaffold(),
+    );
+  }
+}
---
+++ eventsapp/lib/themes/light_theme.dart
@@ -0,0 +1,7 @@
+import 'package:eventsapp/resources/colors.dart';
+import 'package:flutter/material.dart';
+
+final lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: AppColors.primaryColor,
+);
