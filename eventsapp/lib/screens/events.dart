c055c4e569e9644de65c4614fef5e61735e2b8f1
Suman Adhikari <expertxkid@gmail.com>
2021-11-20 18:39:23 +0545

    Add bottom navigation bar

--- eventsapp/lib/screens/events.dart
+++ eventsapp/lib/screens/events.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class EventsScreen extends StatelessWidget {
   const EventsScreen({Key? key}) : super(key: key);
@@ -6,7 +7,68 @@ class EventsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Events Page')),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.qr_code_outlined),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.explore), label: 'Explore'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.location_on), label: 'Map'),
+            BottomNavigationBarItem(icon: Icon(Icons.piano), label: 'Ticket'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 60, left: 16, right: 16, bottom: 50),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: 'Search'),
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Upcoming events',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 32,
+                    ),
+                  )),
+              SizedBox(
+                height: 250,
+                width: double.infinity,
+                child: ListView.builder(
+                  physics: BouncingScrollPhysics(),
+                  padding: EdgeInsets.only(
+                    left: 16,
+                    right: 16,
+                  ),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Text('asd');
+                  },
+                  itemCount: 20,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
