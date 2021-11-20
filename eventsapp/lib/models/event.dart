4cdf5aa7c02ec783a45966c8aee7bf06d090a033
Suman Adhikari <expertxkid@gmail.com>
2021-11-20 21:05:47 +0545

    Update event model

--- eventsapp/lib/models/event.dart
+++ eventsapp/lib/models/event.dart
@@ -1,7 +1,19 @@
 class Event {
   String imageLocation;
+  String name;
+  String location;
+  DateTime startDateTime;
+  DateTime endDateTime;
+  String organizerName;
+  double priceInDollars;
 
   Event({
     required this.imageLocation,
+    required this.name,
+    required this.location,
+    required this.startDateTime,
+    required this.endDateTime,
+    required this.organizerName,
+    required this.priceInDollars,
   });
 }
