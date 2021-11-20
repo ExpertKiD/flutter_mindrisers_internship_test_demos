d1ad90045678483a20d4d52cde62f1c5e82abfa9
Suman Adhikari <expertxkid@gmail.com>
2021-11-20 21:15:54 +0545

    Update all widgets in event card with const modifier

--- eventsapp/lib/widgets/event_card.dart
+++ eventsapp/lib/widgets/event_card.dart
@@ -1,3 +1,4 @@
+import 'package:eventsapp/resources/colors.dart';
 import 'package:eventsapp/view_models/event.dart';
 import 'package:flutter/material.dart';
 
@@ -8,9 +9,87 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
+      width: 313,
+      height: 250,
       child: Column(
-        children: [Image.asset(eventModel.imageLocation)],
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(
+              eventModel.imageLocation,
+              cacheWidth: 313,
+              cacheHeight: 169,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Row(
+            children: [
+              SizedBox(
+                width: 46,
+                height: 46,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Color(0xFFF1E8FB),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Jul',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.golden,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        '13',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'The Pretty Reckless',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.location_on,
+                        color: AppColors.primaryColor,
+                      ),
+                      Text(
+                        'Fairview Gospel Church',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
