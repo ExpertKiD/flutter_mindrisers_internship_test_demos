1c83b0ebdeb95749ccff60dff129c100ca49b802
Suman Adhikari <expertxkid@gmail.com>
2021-11-20 21:06:15 +0545

    Update event view model to match event model fields

--- eventsapp/lib/view_models/event.dart
+++ eventsapp/lib/view_models/event.dart
@@ -3,13 +3,31 @@ import 'package:eventsapp/models/event.dart';
 class EventModel extends Event {
   EventModel({
     required imageLocation,
+    required name,
+    required location,
+    required startDateTime,
+    required endDateTime,
+    required organizerName,
+    required priceInDollars,
   }) : super(
           imageLocation: imageLocation,
+          name: name,
+          location: location,
+          startDateTime: startDateTime,
+          endDateTime: endDateTime,
+          organizerName: organizerName,
+          priceInDollars: priceInDollars,
         );
 
   factory EventModel.from(Event event) {
     return EventModel(
       imageLocation: event.imageLocation,
+      name: event.name,
+      location: event.location,
+      startDateTime: event.startDateTime,
+      endDateTime: event.endDateTime,
+      organizerName: event.organizerName,
+      priceInDollars: event.priceInDollars,
     );
   }
 }
