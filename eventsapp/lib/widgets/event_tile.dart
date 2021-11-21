8f72c779fe7ca0c876dacaab1fcfa689b3329526
Suman Adhikari <expertxkid@gmail.com>
2021-11-21 13:14:39 +0545

    Update event tile widget to match event view model

---
+++ eventsapp/lib/widgets/event_tile.dart
@@ -0,0 +1,56 @@
+import 'package:eventsapp/resources/colors.dart';
+import 'package:eventsapp/view_models/event.dart';
+import 'package:flutter/material.dart';
+
+class EventTile extends StatelessWidget {
+  final EventModel eventModel;
+
+  const EventTile({Key? key, required this.eventModel}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {},
+      title: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            eventModel.getEventFullDateWithMonthAbbr(),
+            style: const TextStyle(fontSize: 12, color: AppColors.golden),
+          ),
+          Text(
+            eventModel.name,
+            style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: AppColors.black),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.location_on,
+                size: 10,
+              ),
+              Text(
+                eventModel.getEventFullDateWithMonthAbbr(),
+                style: const TextStyle(fontSize: 10, color: AppColors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+      //isThreeLine: true,
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.asset(
+          eventModel.imageLocation,
+          cacheWidth: 100,
+          cacheHeight: 100,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
