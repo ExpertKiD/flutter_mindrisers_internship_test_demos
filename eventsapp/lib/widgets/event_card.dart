841aaaf3024a48c95a83fca0c57ce648cd8e06bc
Suman Adhikari <expertxkid@gmail.com>
2021-11-21 13:36:22 +0545

    Add inkwell to event card widget

--- eventsapp/lib/widgets/event_card.dart
+++ eventsapp/lib/widgets/event_card.dart
@@ -4,92 +4,100 @@ import 'package:flutter/material.dart';
 
 class EventCard extends StatelessWidget {
   final EventModel eventModel;
+  final void Function()? callback;
 
-  const EventCard({Key? key, required this.eventModel}) : super(key: key);
+  const EventCard({
+    Key? key,
+    required this.eventModel,
+    this.callback,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 313,
       height: 250,
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(16),
-            child: Image.asset(
-              eventModel.imageLocation,
-              cacheWidth: 313,
-              cacheHeight: 169,
-              fit: BoxFit.cover,
+      child: InkWell(
+        onTap: callback,
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.asset(
+                eventModel.imageLocation,
+                cacheWidth: 313,
+                cacheHeight: 169,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          const SizedBox(height: 25),
-          Row(
-            children: [
-              SizedBox(
-                width: 46,
-                height: 46,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: AppColors.softHighlightColor,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        eventModel.getEventMonthAbbr(),
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: AppColors.golden,
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                SizedBox(
+                  width: 46,
+                  height: 46,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      color: AppColors.softHighlightColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          eventModel.getEventMonthAbbr(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.golden,
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 2,
-                      ),
-                      Text(
-                        eventModel.getEventDay(),
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+                        const SizedBox(
+                          height: 2,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 16,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    eventModel.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
+                        Text(
+                          eventModel.getEventDay(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.location_on,
-                        color: AppColors.primaryColor,
+                ),
+                const SizedBox(
+                  width: 16,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      eventModel.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
                       ),
-                      Text(
-                        eventModel.location,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 14,
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.location_on,
+                          color: AppColors.primaryColor,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          )
-        ],
+                        Text(
+                          eventModel.location,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
